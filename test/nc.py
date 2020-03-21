57c92b9c8214d6fcfe23b49d2eb13d8d4400b8c5
CurryRice233 <nmeia@qq.com>
2020-03-21 11:43:46 +0100

    delete test

--- test/nc.py
+++
@@ -1,9 +0,0 @@
-class Nc(object):
-    def __init__(self, title, ncid, link, size):
-        self.title = title
-        self.ncid = ncid
-        self.link = link
-        self.size = size
-
-    def __str__(self):
-        print('\n' + self.title + '\n' + self.ncid + '\n' + self.link + '\n' + self.size)
