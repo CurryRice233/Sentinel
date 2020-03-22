ee15095357ab634f7c5e68a6fda0f25a22fa1695
CurryRice233 <nmeia@qq.com>
2020-03-22 13:07:40 +0100

    update git with ssh

--- github.py
+++ github.py
@@ -1,14 +1,17 @@
 from git import Repo
+from git import Git
 import datetime
 
 
 def push_to_github():
-    repo = Repo(".")
-    remote = repo.remote()
-    remote.pull()
+    git_ssh_cmd = 'ssh -i %s id_rsa'
+    with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
+        repo = Repo(".")
+        remote = repo.remote()
+        remote.pull()
 
-    git = repo.git
-    git.add("docs")
-    git.commit("-m", "update by python script " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        git = repo.git
+        git.add("docs")
+        git.commit("-m", "update by python script " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    remote.push()
+        remote.push()
