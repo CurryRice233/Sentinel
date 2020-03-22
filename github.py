3ace5effa1dcefa8336fec89ece746aaa6c68a4f
CurryRice233 <nmeia@qq.com>
2020-03-22 18:17:21 +0100

    push to github

--- github.py
+++ github.py
@@ -1,16 +1,20 @@
 from git import Repo
+from git import Git
 import datetime
-import subprocess
+import os
 
 
 def push_to_github():
-    repo = Repo(".")
-    git = repo.git
-    remote = repo.remote()
-    remote.pull()
+    git_ssh_identity_file = os.path.expanduser('~/.ssh/id_rsa')
+    git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
+    with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
+        repo = Repo(".")
+        git = repo.git
+        remote = repo.remote()
+        remote.pull()
 
-    git.add("docs")
-    git.commit("-m", "update by python script " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        git.add("docs")
+        git.commit("-m", "update by python script " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    remote.push()
+        remote.push()
 
